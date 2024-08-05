e35f02107fd611d19700ba6cddb4f97b66a0aa36
TsuriHefer <tsuri.he@gmail.com>
2024-08-05 21:30:54 +0300

    WIP working on editInvoice

--- src/Components/Layout/InvoiceArea/EditInvoice/EditInvoice.tsx
+++ src/Components/Layout/InvoiceArea/EditInvoice/EditInvoice.tsx
@@ -1,4 +1,6 @@
 import "./EditInvoice.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 
 export function EditInvoice(): JSX.Element {
 
@@ -41,9 +43,83 @@ export function EditInvoice(): JSX.Element {
 
             <div className="bill-to-container">
 
+                <p className="bill-to headline">Bill To</p>
+
+                <label htmlFor="" className="client-name">Client's Name
+                    <input type="text" />
+                </label>
+
+                <label htmlFor="" className="client-email">Client's Email
+                    <input type="text" />
+                </label>
+
+                <label htmlFor="" className="street-address">Street Address
+                    <input type="text" />
+                </label>
+
+                <div className="city-country-container">
+
+                    <div className="city-and-post-container">
+
+                        <label htmlFor="" className="city">City
+                            <input type="text" />
+                        </label>
+
+                        <label htmlFor="" className="post-code">Post Code
+                            <input type="text" />
+                        </label>
+
+                    </div>
+
+                    <label htmlFor="" className="country">Country
+                        <input type="text" />
+                    </label>
+
+                </div>
+
+                <div className="last-details">
+
+                    <div className="invoice-date-container">
+
+                        <label htmlFor="" className="invoice-date">Invoice Date
+                            <input type="date" />
+                        </label>
+
+                        <label htmlFor="" className="payment-terms">Payment Terms
+                            <select name="" id="">
+                                <option value="">Net 30 Days</option>
+                                <option value="">Net 60 Days</option>
+                                <option value="">Net 90 Days</option>
+                            </select>
+                        </label>
+
+
+                    </div>
+
+                    <label htmlFor="" className="project-name">Project Description
+                        <input type="text" />
+                    </label>
+
+                </div>
+
+
             </div>
 
             <div className="items-container">
+
+                <p>Item Name</p>
+                <p>Qty.</p>
+                <p>Price</p>
+                <p>Total</p>
+                <FontAwesomeIcon icon={faTrash} className="space-filler" />
+
+               
+
+                <input type="text" placeholder="test" />
+                <input type="text" placeholder="test"/>
+                <input type="text" placeholder="test" />
+                <input type="text" placeholder="test"/>
+                <FontAwesomeIcon icon={faTrash} />
 
             </div>
 
